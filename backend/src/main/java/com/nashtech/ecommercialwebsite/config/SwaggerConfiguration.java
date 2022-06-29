aa2fff103442db87641bdd9e7ebe20740eafd260
quocminhh47 <quocminhh47@gmail.com>
2022-06-29 22:59:38 +0700

    Add: Open API - Swagger UI configuraton demo

---
+++ backend/src/main/java/com/nashtech/ecommercialwebsite/config/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package com.nashtech.ecommercialwebsite.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Value("${prop.swagger.enabled:true}")
+    private boolean enableSwagger;
+
+
+
+
+    @Bean
+    public Docket SwaggerConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(enableSwagger)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.nashtech.ecommercialwebsite.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (enableSwagger) {
+            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+}
--- backend/src/main/java/com/nashtech/ecommercialwebsite/config/WebSecurityConfig.java
+++ backend/src/main/java/com/nashtech/ecommercialwebsite/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.nashtech.ecommercialwebsite.config;
 import com.nashtech.ecommercialwebsite.security.filter.CustomAuthenticationFilter;
 import com.nashtech.ecommercialwebsite.security.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,24 +27,54 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Value("${prop.swagger.enabled:true}")
+    private boolean enableSwagger;
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/swagger-resources/configuration/ui",
+            "/swagger-resources/configuration/ui",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        if (enableSwagger)
+            web.ignoring().antMatchers(AUTH_WHITELIST);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter =
                 new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(
                 "/api/login/**",
                 "/token/refresh/**",
                 "/v1/api/registration/**").permitAll();
-        http.authorizeRequests().antMatchers( "/admin/api/**").hasRole("ADMIN");
-        http.authorizeRequests().antMatchers( "/api/ratings/**",
-                "/api/products/gallery/**").hasRole("USER");
-        http.authorizeRequests().anyRequest().authenticated();
+
+        http.authorizeRequests().antMatchers("/admin/api/**").permitAll();//hasRole("ADMIN");
+
+        http.authorizeRequests().antMatchers("/api/ratings/**",
+                "/api/products/gallery/**").permitAll();//hasRole("USER");
+       /* http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);*/
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -52,7 +84,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 }
